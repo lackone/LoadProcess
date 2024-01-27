3a5ea5aeb09ab435d6ce5e99a757bd3cc4d8f821
lackone <lackone@126.com>
2024-01-28 00:24:05 +0800

    修改

--- 内存写入_写入方/PETools.h
+++ 内存写入_写入方/PETools.h
@@ -6,9 +6,9 @@
 class PETools
 {
 public:
-	//修正重定位表
-	VOID ReviseRelocation(IN LPVOID pImageBuffer, IN DWORD offset);
 	//修复IAT表
 	VOID RepairIAT(IN LPVOID pImageBuffer);
+	//修正重定位表
+	VOID ReviseRelocation(IN LPVOID pImageBuffer, IN DWORD offset);
 };
 
